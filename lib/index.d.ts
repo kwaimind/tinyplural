a1c5f1cfcd714c64e0e7241cb01e2c1877946059
Daniel Reed <danieel.reed@gmail.com>
2020-08-12 21:31:43 +0200

    see if this works

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -4,5 +4,6 @@ export declare const isNonChanging: (noun: string) => stringReturn;
 export declare const isONNoun: (noun: string, count?: number) => stringReturn;
 export declare const endsInO: (noun: string, count?: number) => stringReturn;
 export declare const endsInY: (noun: string, count?: number) => stringReturn;
+export declare const endsInFOrFe: (noun: string, count?: number) => stringReturn;
 declare const makeSuffix: (noun: string, count?: number) => string;
 export default makeSuffix;
