c2731e8405dd31d96061b38d6030ad0ccfe9d1a5
Daniel Reed <danieel.reed@gmail.com>
2021-06-30 15:44:37 +0200

    Feature/reuse funcs (#18)
    * v1.1.3
    
    * cleanup to reuse functions
    
    * cleanup

--- src/types.ts
+++ src/types.ts
@@ -4,3 +4,17 @@ export interface NounObject {
   single: string;
   plural: string;
 }
+
+export type SimpleFunction = (noun: string, count?: number) => SuffixReturn;
+
+export type FindAndReplaceFunc = (
+  noun: string,
+  find: string,
+  replace: string
+) => SuffixReturn;
+
+export interface FunctionWithOptions {
+  action: FindAndReplaceFunc;
+  findKey: string;
+  endKey: string;
+}
--- test/endsInIs.test.ts
+++
@@ -1,9 +0,0 @@
-import endsInIs from '../src/endsInIs';
-
-describe('When testing endsInIs', () => {
-  it('returns i for nouns ending in us', () => {
-    expect(endsInIs('analysis')).toEqual('analyses');
-    expect(endsInIs('basis')).toEqual('bases');
-    expect(endsInIs('crisis')).toEqual('crises');
-  });
-});
